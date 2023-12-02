3c7d81888fd5ca55cc3807f7d90c68607416d836
Nilay Das <mailnilaydas@gmail.com>
2023-12-02 18:35:30 +0530

    1st commit

---
+++ src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping()
+    public String sayHello()
+    {
+        return "index";
+    }
+
+    @GetMapping("/list")
+    public String showCustomers()
+    {
+        return "list-customers";
+    }
+}
